af624af7f6fa6a8202408db52ff14fdf6aa0d0fc
mbuschgens <marco.buschgens@gmail.com>
2015-08-11 16:11:53 +0200

    android

--- www/js/sslnote.common.js
+++ www/js/sslnote.common.js
@@ -1,43 +1,6 @@
-// Determine theme depending on device
-var isAndroid = Framework7.prototype.device.android === true;
-var isIos = Framework7.prototype.device.ios === true;
- 
-// Set Template7 global devices flags
-Template7.global = {
-    android: isAndroid,
-    ios: isIos
-};
- 
-// Define Dom7
+// @codekit-prepend "jquery.js"
+'use strict';
 var $$ = Dom7;
- 
-// Add CSS Styles
-if (isAndroid) {
-    $$('head').append(
-        '<link rel="stylesheet" href="css/framework7.material.min.css">' +
-        '<link rel="stylesheet" href="css/framework7.material.colors.min.css">' +
-        '<link rel="stylesheet" href="css/pem.css">'
-    );
-}
-else {
-    $$('head').append(
-        '<link rel="stylesheet" href="css/framework7.ios.min.css">' +
-        '<link rel="stylesheet" href="css/framework7.ios.colors.min.css">' +
-        '<link rel="stylesheet" href="css/pem.css">'
-    );
-}
- 
-// Change Through navbar layout to Fixed
-if (isAndroid) {
-    // Change class
-    $$('.view.navbar-through').removeClass('navbar-through').addClass('navbar-fixed');
-    // And move Navbar into Page
-    $$('.view .navbar').prependTo('.view .page');
-}
- 
-
-
-
 var sslnote = new Framework7({
     debug: true,
     //cache: true,
@@ -48,10 +11,6 @@ var sslnote = new Framework7({
     swipeBackPageAnimateOpacity: false,
     modalTitle: unescape('PEM'),
     router: true,
-        // Enable Material theme for Android device only
-    material: isAndroid ? true : false,
-    // Enable Template7 pages
-    template7Pages: true,
     //reloadPages:true,
     onAjaxStart: function(xhr) {
         sslnote.showIndicator();
